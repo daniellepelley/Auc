c9550d7cab1b660ae873bff01f2e5713a7efffb3
Daniel Le Pelley <daniellepelley@gmail.com>
2015-03-06 06:19:50 +0000

    Added Auction domain objects

---
+++ Auction.Reporting.DomainModel/Auction.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Auction.Reporting.DomainModel
+{
+    public class Auction
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Sale> Sales { get; set; }
+        public int AuctionHouseId { get; set; }
+        public AuctionHouse AuctionHouse { get; set; }
+    }
+}
---
+++ Auction.Reporting.DomainModel/AuctionHouse.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Auction.Reporting.DomainModel
+{
+    public class AuctionHouse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Auction> Auctions { get; set; }
+    }
+}
